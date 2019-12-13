13e8e2ec7fc36bf94a5eb442c92c6e8b5092171c
Glenn Strauss <gstrauss@gluelogic.com>
2019-12-13 14:52:56 -0500

    retry (later) on read() or write() if EINTR,EAGAIN
    fixes bug whereby interrupted read()/write() could corrupt data stream

--- bin/knc.c
+++ bin/knc.c
@@ -770,7 +770,7 @@ move_network_to_local_buffer(work_t *work)
 		LOG(LOG_ERR, ("gstd_read error"));
 		return -1;
 	case -2:
-		return 1;
+		return 1; /* retry later (do not return <= 0) */
 	}
 
 	work->local_buffer.in_valid = 1;
@@ -795,6 +795,8 @@ move_local_to_network_buffer(work_t *work)
 		/* EOF */
 		return 0;
 	} else if (work->network_buffer.in_len < 0) {
+		if (errno == EINTR || errno == EAGAIN)
+			return 1; /* retry later (do not return <= 0) */
 		if (errno == ECONNRESET)
 			return 0; /* Treat as EOF */
 
@@ -846,7 +848,9 @@ write_local_buffer(work_t *work)
 		    &(work->local_buffer.out[work->local_buffer.out_pos]),
 		    work->local_buffer.out_len);
 
-	if (len < 0 && ((errno != EINTR) && (errno != EAGAIN))) {
+	if (len < 0) {
+		if (errno == EINTR || errno == EAGAIN)
+			return 1; /* retry later (do not return <= 0) */
 		if (errno == EPIPE) {
 			/*
 			 * It's possible that exec'd programs (or the
@@ -955,7 +959,9 @@ write_network_buffer(work_t *work)
 		    &(work->network_buffer.out[work->network_buffer.out_pos]),
 		    work->network_buffer.out_len);
 
-	if (len < 0 && ((errno != EINTR) && (errno != EAGAIN))) {
+	if (len < 0) {
+		if (errno == EINTR || errno == EAGAIN)
+			return 1; /* retry later (do not return <= 0) */
 		if (errno == EPIPE) {
 			/*
 			 * It's possible that exec'd programs (or the
@@ -1149,9 +1155,11 @@ move_data(work_t *work)
 				mret = read(work->local_err, errbuf,
 					    sizeof(errbuf) - 1);
 				switch (mret) {
-				case 0:
-					/*FALLTHROUGH*/
 				case -1:
+					if (errno == EINTR || errno == EAGAIN)
+						break;
+					/*FALLTHROUGH*/
+				case 0:
 					/* just close it on errors or EOF. */
 					close(work->local_err);
 					work->local_err = -1;
